c82cb2c6027ca749793ded7a89d7638f8c38c53e
23ECM01-Eros-Cajas <ecajasdb23@gmail.com>
2025-03-24 11:55:42 -0600

    Add files via upload

---
+++ Guia_1_ 24 _de_marzo 03 Eros Cajas/Form3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia_1__24__de_marzo_03_Eros_Cajas
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cont3 = 2;
+            int num1 = int.Parse(textBox1.Text);
+            do
+            {
+                int multi;
+                multi = num1 * cont3;
+                dataGridView1.Rows.Add(num1, cont3, multi);
+                cont3++;
+            }
+            while (cont3 < 11);
+
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form4 form = new Form4();
+            form.ShowDialog();
+        }
+    }
+}
---
+++ Guia_1_ 24 _de_marzo 03 Eros Cajas/Form4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia_1__24__de_marzo_03_Eros_Cajas
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+
+            for (int cont = 0; cont < 10; cont++)
+            {
+                int suma;
+                suma = num1 + num2 + cont;
+
+                dataGridView1.Rows.Add(num1, num2,cont, suma);
+                cont++;
+            }
+        }
+    }
+}
